8340a39911848323c638cc71d4a1da3f51975497
celso yamashita <ctyamashita@hotmail.com>
2025-11-20 17:09:12 +0900

    adding source list

--- decibel.js
+++ decibel.js
@@ -1,7 +1,7 @@
 import DecibelMeter from 'decibel-meter'
 
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ audio: true }).then(()=>{
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream)=>{    
     const dbMeterDisplay = document.querySelector('#db-meter')
     const visualDisplay = document.getElementById('display')
     const realTime = document.getElementById('real-time')
@@ -19,11 +19,14 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     let intervalOfChecks = 100
     let intervalOfChecksLoop
     
-    function resetDbMeter(dbMeter) {
+    async function resetDbMeter(dbMeter) {
       const checkTitle = document.getElementById('check-title')
       checkTitle.innerHTML = `Latest collection <small>(${delayCheck/ intervalOfChecks} checks every ${delayCheck/1000}s)</small>`
       dbMeter?.disconnect()
       dbMeter = new DecibelMeter
+      const sources = await dbMeter.sources
+      const sourcesNames = sources.map((src,index)=>`<p>[${index}] ${src.label}</p>`).join('')
+      document.body.insertAdjacentHTML('beforeend', '<h3>Sources</h3>' + sourcesNames)
       dbMeter.listenTo(0, (dB, percent, value) => {
         dbMeterData = { dB: dB, percent: percent, value: value }
       })
@@ -37,9 +40,9 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       dbMeter.on('connect', ()=>{
         // collecting dB measurements
         intervalOfChecksLoop = setInterval(() => {
-          const { dB, percent, value } = dbMeterData
+          const { dB } = dbMeterData
           const dbDiff = Math.abs(Math.round(dB - prevDb))
-          soundHistory.push(Math.abs(Math.round(dB)))
+          soundHistory.push(Math.abs(Math.round(dbDiff)))
     
           // for circle animation
           const size = Math.round(((dbDiff / height) + 1) * height)
@@ -59,6 +62,7 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
           const pastFiveSec = soundHistory.slice(-1 * (delayCheck / intervalOfChecks))
           const dBAverage = pastFiveSec.reduce((a,b)=>a+b) / (delayCheck / intervalOfChecks)
           // soundDetected = dBAverage < 94.5
+          console.log(dBAverage)
           soundDetected = dBAverage < dBlimit
     
           // updating color
@@ -88,7 +92,9 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     let dbMeter = resetDbMeter()
     // update if interval input changes
     const intervalInput = document.getElementById('interval')
+    intervalInput.value = intervalOfChecks
     const intervalDisplay = document.getElementById('intervalDisplay')
+    intervalDisplay.innerHTML = intervalOfChecks + " <small>msec</small>"
     intervalInput.addEventListener('change', (e)=>{
       intervalOfChecks = e.currentTarget.value
       dbMeter = resetDbMeter(dbMeter)
@@ -99,6 +105,7 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     })
     // update if check input changes
     const checkInput = document.getElementById('check')
+    checkInput.value = delayCheck
     const checkDisplay = document.getElementById('checkDisplay')
     checkInput.addEventListener('change', (e)=>{
       delayCheck = e.currentTarget.value
@@ -111,6 +118,7 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 
     // update if db limit input changes
     const dBlimitInput = document.getElementById('dBlimit')
+    dBlimitInput.value = dBlimit
     const dBlimitDisplay = document.getElementById('dBlimitDisplay')
     dBlimitInput.addEventListener('change', (e)=>{
       dBlimit = e.currentTarget.value
